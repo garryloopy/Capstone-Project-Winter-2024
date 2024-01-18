4eb779b11243b3243acaa789eff4fd4c4be211af
Garry Jr Dayag <garrydayagjr@gmail.com>
2024-01-18 14:26:14 -0700

    Minor changes to header and footer

--- components/Footer.js
+++ components/Footer.js
@@ -5,6 +5,7 @@
 export default function Footer() {
     return (
         <footer>
+            <h1>Footer</h1>
         </footer>
     )
 }
--- components/Header.js
+++ components/Header.js
@@ -5,7 +5,7 @@
 export default function Header() {
     return (
         <div>
-
+            <h1>Header</h1>
         </div>
     )
 }
