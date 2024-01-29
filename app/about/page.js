362f86cd4a4fb05f0089fcb3017fc09021b7d823
Kevin <homingkevin.wong@edu.sait.ca>
2024-01-29 09:47:18 -0700

    Update about/page.js

--- app/about/page.js
+++ app/about/page.js
@@ -1,28 +1,20 @@
-import React from "react";
-
 const AboutPage = () => {
   return (
-    <div className="flex lg:flex-row flex-col items-center justify-evenly gap-10 lg:w-[80%] mx-auto w-full ">
+    <div className="flex flex-col items-center justify-evenly gap-10 lg:w-[80%] mx-auto w-full text-white text-center">
       <p>AboutPage</p>
       <p>Development Team: </p>
-      <ul className="text-white text-center">
+      <ul>
         <li>
           Garry Jr Dayag (Main Developer - Front-end) | Email:
           garryjr.dayag@edu.sait.ca
         </li>
-      </ul>
-      <ul className="text-white text-center">
         <li>
           Rimon Alqoshi (Main Developer - Back-end) | Email:
           rimon.alqoshi@edu.sait.ca
         </li>
-      </ul>
-      <ul className="text-white text-center">
         <li>
           Edward Montilla (UI/UX Designer) | Email: edward.montilla@edu.sait.ca
         </li>
-      </ul>
-      <ul className="text-white text-center">
         <li>
           Kevin Wong (Associate Developer) | Email: homingkevin.wong@edu.sait.ca
         </li>
