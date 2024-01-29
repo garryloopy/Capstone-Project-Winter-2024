1a1fd5324843f39de5d8bc6ae97772341c1678c0
Garry Jr Dayag <garrydayagjr@gmail.com>
2024-01-29 11:11:11 -0700

    Added modal for the contact page as a confirmation

--- app/contact/page.js
+++ app/contact/page.js
@@ -11,12 +11,22 @@ import React from "react";
 // The business email
 const BUSINESS_EMAIL = "francessicam@gmail.com";
 
+const defaultSubmittedUser = {
+  name: "",
+  email: "",
+  phoneNumber: "",
+  message: "",
+};
+
 const ContactPage = () => {
   // States for the form
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [phoneNumber, setPhoneNumber] = useState("");
   const [message, setMessage] = useState("");
+
+  const [submittedUser, setSubmittedUser] = useState(defaultSubmittedUser);
+  const [showConfirmation, setShowConfirmation] = useState(false);
 
   /**
    * Handler for the name change
@@ -68,16 +78,48 @@ const ContactPage = () => {
   const handleOnSubmit = (e) => {
     e.preventDefault();
 
-    // Used for debugging
-    console.log("Name: ", name);
-    console.log("Email: ", email);
-    console.log("Phone number: ", phoneNumber);
-    console.log("Message: ", message);
+    //Do some stuff, send email to the business email
+
+    //Show confirmation
+    setSubmittedUser({
+      name: name,
+      email: email,
+      phoneNumber: phoneNumber,
+      message: message,
+    });
+    setShowConfirmation(true);
 
     resetValue();
   };
+
+  /**
+   * Handler closing the confirmation modal
+   */
+  const handleOnCloseConfirmation = () => {
+    setShowConfirmation(false);
+
+    setSubmittedUser(defaultSubmittedUser);
+  };
+
   return (
     <div className="flex flex-col justify-center h-screen gap-12 items-center">
+      {showConfirmation && (
+        <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center backdrop-filter backdrop-blur-sm">
+          <div className="flex- flex-col bg-white/75 p-8 rounded-md">
+            <div className="text-right">
+              <button onClick={handleOnCloseConfirmation}>X</button>
+            </div>
+            <div>
+              <p>Thank you, {submittedUser.name}.</p>
+              <p>
+                A confirmation email has been sent to {submittedUser.email}.
+              </p>
+              <p>We will get back to you in 3-5 business days.</p>
+            </div>
+          </div>
+        </div>
+      )}
+
       <form onSubmit={handleOnSubmit} className="flex flex-col gap-4 p-12">
         <p className="text-center text-2xl mb-4">Get in touch with us!</p>
         <div className="flex flex-row gap-8">
