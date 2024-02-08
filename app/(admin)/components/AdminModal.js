5f83e0fe152d53b2dd342ec9ff54a4529e00f2e7
Edward Montilla <edward.montilla@edu.sait.ca>
2024-02-07 17:19:22 -0700

    Update AdminModal.js

--- app/(admin)/components/AdminModal.js
+++ app/(admin)/components/AdminModal.js
@@ -12,17 +12,17 @@ const AdminModal = () => {
     <>
       {showModal && (
         <div className="fixed bottom-0 right-[.1rem] lg:mr-4 lg:mb-4 text-white">
-          <div className="bg-black/60 p-4 rounded-md shadow-md border flex flex-col ">
+          <div className="bg-black/60 p-2 rounded-md shadow-md border flex flex-col ">
             <span
-              className="text-gray-600 font-bold text-2xl p-1 hover:bg-red-400 cursor-pointer self-end"
+              className="text-gray-500 font-bold text-2xl p-2 pt-1 pb-1 rounded-md hover:bg-red-400 cursor-pointer self-end"
               onClick={handleClose}
             >
               &times;
             </span>
-            <div className="p-4 flex flex-col justify-center items-center">
-              <p>Sign in as an admin to access the dashboard.</p>
+            <div className="p-2 flex flex-col justify-center items-center">
+              <p>Sign in as admin to access the dashboard.</p>
               <Link
-                className="px-4 py-2 border border-gray-400 rounded-md my-6 hover:bg-white hover:text-black"
+                className="px-4 py-1 border border-gray-400 rounded-md my-6 hover:bg-white hover:text-black"
                 href="/sign-in"
               >
                 Sign in
