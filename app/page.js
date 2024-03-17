23a12dbbf2d46554f1b075b51aa2c6f03e8a8da9
Rimon83 <111208690+Rimon83@users.noreply.github.com>
2024-03-17 15:58:54 -0600

    update SingleMenu component and home page

--- app/page.js
+++ app/page.js
@@ -5,15 +5,23 @@ import { useState } from "react";
 import AdminModal from "./(admin)/components/AdminModal";
 import NewFlavor from "@/components/NewFlavor";
 import About from "@/components/About";
+import { useSession } from "next-auth/react";
 export default function HomePage() {
+  const session = useSession();
+  const {status} = session;
   return (
     <section className="md:px-[4rem] px-[2rem] py-[2rem] text-white flex flex-col items-center justify-center my-[6rem]">
       <NewFlavor />
       <About />
 
-      <div>
-        <AdminModal />
-      </div>
+      {status === "unauthenticated" ? (
+        <div>
+          <AdminModal />
+        </div>
+      ) : (
+        null
+      )}
     </section>
   );
 }
+
