5ba1cbc203089338ba613652ece741e5af4198d7
Edward Montilla <edward.montilla@edu.sait.ca>
2024-03-26 19:16:53 -0600

    slides in progress

--- app/page.js
+++ app/page.js
@@ -12,7 +12,9 @@ export default function HomePage() {
   const {status} = session;
   return (
     <section className="md:px-[4rem] px-[2rem] py-[2rem] text-white flex flex-col items-center justify-center my-[6rem]">
+      <div className="slide-container">
       <SlideContent />
+      </div>
       {/* <NewFlavor /> */}
       <About />
 
--- components/SlideContent.js
+++ components/SlideContent.js
@@ -1,12 +1,23 @@
-
 import React from "react";
 
-const SlideContent = ({ imageUrl, slideText }) => {
+const SlideContent = () => {
+  const slideImages = [
+    { imageUrl: "/images/rimon-logo.png", slideText: "slide 1" },
+    { imageUrl: "/images/background.jpg", slideText: "slide 2" },
+    { imageUrl: "/images/gallery01.png", slideText: "slide 3" },
+    { imageUrl: "/images/gallery02.png", slideText: "slide 4" },
+    // Add more image URLs and slide texts as needed
+  ];
+
   return (
-    <div className="each-slide-effect">
-      <div style={{ backgroundImage: `url(${imageUrl})` }}>
-        <span>{slideText}</span>
-      </div>
+    <div>
+      {slideImages.map((slide, index) => (
+        <div key={index} className="each-slide-effect">
+          <div style={{ backgroundImage: `url(${slide.imageUrl})` }}>
+            <span>{slide.slideText}</span>
+          </div>
+        </div>
+      ))}
     </div>
   );
 };
