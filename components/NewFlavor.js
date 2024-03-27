a40f2147fd62958cd00d1f05f9c089146246bf61
edwardmontilla <119634477+edwardmontilla@users.noreply.github.com>
2024-03-27 15:50:28 -0700

    Update NewFlavor.js

--- components/NewFlavor.js
+++ components/NewFlavor.js
@@ -1,43 +1,19 @@
-// import React from 'react'
-// import {
-//   BsArrowLeftShort,
-//   BsArrowRightShort,
-// } from "react-icons/bs";
-// import Link from "next/link";
+import React from 'react'
+import {
+  BsArrowLeftShort,
+  BsArrowRightShort,
+} from "react-icons/bs";
+import Link from "next/link";
 
-// const images = [
-//   "/images/gallery01.png",
-//   "/images/gallery02.png",
-//   "/images/gallery03.png",
-// ];
+const images = [
+  "/images/gallery01.png",
+  "/images/gallery02.png",
+  "/images/gallery03.png",
+];
 
-// const NewFlavor = () => {
-//   const scrollRef = React.useRef(null);
+const NewFlavor = () => {
+  const scrollRef = React.useRef(null);
 
-//   const scroll = (direction) => {
-//     const { current } = scrollRef;
-//     if (direction === "left") {
-//       current.scrollLeft -= 300;
-//     } else {
-//       current.scrollLeft += 300;
-//     }
-//   };
-//   return (
-//     <div className="gallery flex justify-center items-center mb-[4rem]">
-//       <div className="gallery-content">
-//         <h1 className="headtext__cormorant">Discover New Flavors</h1>
-//         <p className="text-gray-400 my-[2rem]">
-//           {" "}
-//           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-//           eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-//           minim
-//         </p>
-//         <div className="my-[2rem]">
-//           <Link className="btnStyle" href="/menu">
-//             Order Now
-//           </Link>
-//         </div>
-//       </div>
   const scroll = (direction) => {
     const { current } = scrollRef;
     if (direction === "left") {
@@ -47,7 +23,7 @@
     }
   };
   return (
-    <div className="gallery">
+    <div className="gallery flex justify-center items-center mb-[4rem]">
       <div className="gallery-content">
         <h1 className="headtext__cormorant">Discover New Flavors</h1>
         <p className="text-gray-400 my-[2rem]">
@@ -56,44 +32,29 @@
           eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
           minim
         </p>
-        <div className="my-[2rem] hidden md:flex">
+        <div className="my-[2rem]">
           <Link className="btnStyle" href="/menu">
             Order Now
           </Link>
         </div>
       </div>
+      <div className="gallery-images">
+        <div className="gallery-images-container" ref={scrollRef}>
+          {images.map((image, index) => (
+            <div
+              className="gallery-images-card flex justify-center items-center"
+              key={`gallery_image-${index + 1}`}
+            >
+              <img src={image} alt="gallery" />
+              <p className="gallery-desc">
+                {" "}
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut
+              </p>
+            </div>
+          ))}
+        </div>
 
-//       <div className="gallery-images">
-//         <div className="gallery-images-container" ref={scrollRef}>
-//           {images.map((image, index) => (
-//             <div
-//               className="gallery-images-card flex justify-center items-center"
-//               key={`gallery_image-${index + 1}`}
-//             >
-//               <img src={image} alt="gallery" />
-//               <p className="gallery-desc">
-//                 {" "}
-//                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-//                 eiusmod tempor incididunt ut
-//               </p>
-//             </div>
-//           ))}
-//         </div>
-
-//         <div className="gallery-images-arrow">
-//           <BsArrowLeftShort
-//             className="gallery-arrow-icon"
-//             onClick={() => scroll("left")}
-//           />
-//           <BsArrowRightShort
-//             className="gallery-arrow-icon"
-//             onClick={() => scroll("right")}
-//           />
-//         </div>
-//       </div>
-//     </div>
-//   );
-// }
         <div className="gallery-images-arrow">
           <BsArrowLeftShort
             className="gallery-arrow-icon"
@@ -105,13 +66,9 @@
           />
         </div>
       </div>
-      <div className="flex justify-center md:hidden">
-        <Link className="btnStyle" href="/menu">
-          Order Now
-        </Link>
-      </div>
     </div>
   );
 }
 
-// export default NewFlavor
+
+export default NewFlavor
