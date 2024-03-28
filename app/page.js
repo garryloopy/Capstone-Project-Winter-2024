242e464b0193f67dcd68ac22c3d318b166277ad1
edwardmontilla <119634477+edwardmontilla@users.noreply.github.com>
2024-03-27 19:23:39 -0700

    this shiii is hard

--- app/page.js
+++ app/page.js
@@ -3,7 +3,8 @@ import AdminModal from "./(admin)/components/AdminModal";
 import NewFlavor from "@/components/NewFlavor";
 import About from "@/components/About";
 import { useSession } from "next-auth/react";
-import SlideContent from "@/components/SlideContent";
+import Slideshow from "@/components/sliderTest";
+import './slides.css';
 
 
 
@@ -12,8 +13,10 @@ export default function HomePage() {
   const {status} = session;
   return (
     <section className="md:px-[4rem] px-[2rem] py-[2rem] text-white flex flex-col items-center justify-center my-[6rem]">
-
-      <NewFlavor />
+      <div className="slide-container">
+        <Slideshow />
+      </div>
+      {/* <NewFlavor /> */}
 
       <About />
 
