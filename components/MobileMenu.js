de1c441068acf86480ec2653923b444aa9e9a123
Garry Jr Dayag <garrydayagjr@gmail.com>
2024-03-30 13:19:46 -0600

    Update MobileMenu.js

--- components/MobileMenu.js
+++ components/MobileMenu.js
@@ -2,6 +2,7 @@
 import React from "react";
 import Link from "next/link";
 import { signOut, useSession } from "next-auth/react";
+import { CartContext } from "./Providers";
 
 import {
   IoLogOutOutline,
@@ -12,8 +13,10 @@ import {
   IoMailOpenOutline,
 } from "react-icons/io5";
 import { usePathname } from "next/navigation";
+import { useContext } from "react";
 
 const MobileMenu = ({ setToggleMenu }) => {
+  const { cartProducts } = useContext(CartContext);
   const session = useSession();
   const path = usePathname();
   const { name } = session?.data?.user || {};
@@ -28,8 +31,8 @@ const MobileMenu = ({ setToggleMenu }) => {
         <Link
           href="/"
           onClick={() => setToggleMenu(false)}
-          className={`w-full h-12 flex items-center justify-center gap-3 text-slate-800 font-semibold text-xl bg-yellow-400 rounded-lg ${
-            path === "/" ? "opacity-100" : " opacity-75"
+          className={`w-full h-12 flex items-center justify-center gap-3 text-slate-800 font-semibold text-xl bg-yellow-400 rounded-lg hover:bg-yellow-300 active:bg-yellow-400 ${
+            path === "/" ? "opacity-100" : " opacity-80"
           }`}
         >
           <IoHomeOutline size={24} />
@@ -38,8 +41,8 @@ const MobileMenu = ({ setToggleMenu }) => {
         <Link
           href="/menu"
           onClick={() => setToggleMenu(false)}
-          className={`w-full h-12 flex items-center justify-center gap-3 text-slate-800 font-semibold text-xl bg-yellow-400 rounded-lg ${
-            path === "/menu" ? "opacity-100" : " opacity-75"
+          className={`w-full h-12 flex items-center justify-center gap-3 text-slate-800 font-semibold text-xl bg-yellow-400 rounded-lg hover:bg-yellow-300 active:bg-yellow-400 ${
+            path === "/menu" ? "opacity-100" : " opacity-80"
           }`}
         >
           <IoFastFoodOutline size={24} />
@@ -48,17 +51,34 @@ const MobileMenu = ({ setToggleMenu }) => {
         <Link
           href="/contact"
           onClick={() => setToggleMenu(false)}
-          className={`w-full h-12 flex items-center justify-center gap-3 text-slate-800 font-semibold text-xl bg-yellow-400 rounded-lg ${
-            path === "/contact" ? "opacity-100" : " opacity-75"
+          className={`w-full h-12 flex items-center justify-center gap-3 text-slate-800 font-semibold text-xl bg-yellow-400 rounded-lg hover:bg-yellow-300 active:bg-yellow-400 ${
+            path === "/contact" ? "opacity-100" : " opacity-80"
           }`}
         >
           <IoMailOpenOutline size={24} />
           Contact us
         </Link>
+        <Link
+          href="/cart"
+          onClick={() => setToggleMenu(false)}
+          className={`relative w-full h-12 flex items-center justify-center gap-3 text-slate-800 font-semibold text-xl bg-yellow-400 rounded-lg hover:bg-yellow-300 active:bg-yellow-400 ${
+            path === "/cart" ? "opacity-100" : " opacity-80"
+          }`}
+        >
+          <div className="absolute inset-0 flex items-center justify-end px-4 pointer-events-none">
+            {cartProducts?.length > 0 && (
+              <p className="bg-violet-600 size-6 grid place-items-center text-xs rounded-full text-gray-50">
+                {cartProducts?.length}
+              </p>
+            )}
+          </div>
+          <IoCartOutline size={24} />
+          Cart
+        </Link>
       </div>
       {session.status === "authenticated" && (
         <div className="w-full h-max rounded-md overflow-hidden flex flex-col gap-2 bg-gray-600/50 p-4">
-          <div className=" w-full h-full flex flex-col items-center gap-2 pt-8">
+          <div className=" w-full h-full flex flex-col items-center gap-2">
             <h2 className="text-gray-50 font-semibold text-xl border-b w-full text-center border-slate-300 pb-1 mb-4">
               Hello, {username}
             </h2>
@@ -66,22 +86,14 @@ const MobileMenu = ({ setToggleMenu }) => {
             <Link
               href="/menu-list"
               onClick={() => setToggleMenu(false)}
-              className="px-4 py-2 gap-4 h-10 w-full flex flex-row items-center justify-center  bg-yellow-400 rounded-md shadow-md"
+              className="px-4 py-2 gap-4 h-10 w-full flex flex-row items-center justify-center  bg-yellow-400 rounded-md shadow-md hover:bg-yellow-300 active:bg-yellow-400 "
             >
               <IoGridOutline size={24} />
               Dashboard
             </Link>
-            <Link
-              href="/cart"
-              onClick={() => setToggleMenu(false)}
-              className="px-4 py-2 gap-4 h-10 w-full flex flex-row items-center justify-center bg-yellow-400 rounded-md shadow-md"
-            >
-              <IoCartOutline size={24} />
-              Cart
-            </Link>
 
             <button
-              className="px-4 py-2 gap-4 h-10 w-full flex flex-row items-center justify-center bg-yellow-400 rounded-md shadow-md"
+              className="px-4 py-2 gap-4 h-10 w-full flex flex-row items-center justify-center bg-yellow-400 rounded-md shadow-md hover:bg-yellow-300 active:bg-yellow-400 "
               onClick={() => signOut({ callbackUrl: "/" })}
             >
               <IoLogOutOutline size={24} />
