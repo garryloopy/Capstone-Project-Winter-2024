1198bf70e5ed2359ed122388626ec5300c388f3c
Rimon83 <111208690+Rimon83@users.noreply.github.com>
2024-03-30 15:30:50 -0600

    Ensure that adminModel component responds to all sizes of devices

--- app/(admin)/components/AdminModal.js
+++ app/(admin)/components/AdminModal.js
@@ -13,7 +13,7 @@ const AdminModal = () => {
   return (
     <div>
       {showModal && (
-        <div className="fixed bottom-0 right-[.1rem] w-[30rem] h-48 bg-gray-800/95 lg:mr-4 lg:mb-4 text-gray-50 border border-gray-600 rounded-md">
+        <div className="fixed bottom-0 right-0 lg:right-[.1rem] lg:w-[30rem] w-full h-48 bg-gray-800/95 lg:mr-4 lg:mb-4 text-gray-50 border border-gray-600 rounded-md">
           <button
             className="absolute top-6 right-6 size-7 grid place-content-center rounded-md hover:bg-red-400 text-slate-50 hover:text-slate-800"
             onClick={handleClose}
@@ -25,7 +25,7 @@ const AdminModal = () => {
             <p>Sign in as admin to access the dashboard.</p>
             <Link
               href="/sign-in"
-              className="px-4 py-2 gap-4 h-10 w-40 flex flex-row items-center justify-start text-slate-800 font-semibold bg-yellow-400 rounded-md shadow-md"
+              className="gap-4 h-10 w-40 flex flex-row items-center justify-center text-slate-800 font-semibold bg-yellow-400 rounded-md shadow-md"
             >
               <IoLogInOutline size={24} />
               Sign in
