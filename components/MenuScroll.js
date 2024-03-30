67549fdda5a6834cf880291a16cf6ff6bc082848
Rimon83 <111208690+Rimon83@users.noreply.github.com>
2024-03-30 17:30:02 -0600

    Change menuScroll button to yellow

--- components/MenuScroll.js
+++ components/MenuScroll.js
@@ -32,8 +32,8 @@ const MenuScroll = () => {
         <>
             {isVisible && (
                 <button
-                    className="fixed bottom-4 left-1/2 transform -translate-x-1/2 z-50 bg-orange-500 text-white rounded-full w-12 h-12 flex items-center justify-center 
-                    cursor-pointer transition-opacity duration-300 hover:bg-orange-400"
+                    className="fixed bottom-4 left-1/2 transform -translate-x-1/2 z-50 bg-yellow-400 text-black rounded-full w-12 h-12 flex items-center justify-center 
+                    cursor-pointer transition-opacity duration-300 hover:bg-yellow-300"
                     onClick={scrollToTop}
                 >
                     <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
