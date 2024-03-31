93a05c5484aca7918dfede72114a0e38980a5e01
Garry Jr Dayag <garrydayagjr@gmail.com>
2024-03-30 20:39:16 -0600

    Add bg effect on contact form

--- app/contact/page.js
+++ app/contact/page.js
@@ -10,13 +10,13 @@ const BUSINESS_EMAIL = "francessicam@gmail.com";
  */
 const ContactPage = () => {
   return (
-    <div className="flex flex-col justify-center items-center">
+    <section className="flex flex-col justify-center items-center">
       <SubHeader header2="Contact Us" />
 
       <div className="min-h-screen w-full flex justify-center items-start py-12">
         <ContactForm />
       </div>
-    </div>
+    </section>
   );
 };
 
--- components/ClientMenu.js
+++ components/ClientMenu.js
@@ -42,8 +42,10 @@ const ClientMenu = (menuList) => {
       />
 
       {/* used transform/hover:translate to give animation effect for each boxes */}
-      <div className="container relative group text-slate-100 p-6 border rounded-md border-slate-500 shadow-lg 
-      flex flex-col items-center justify-around gap-4 bg-black/60 transform hover:translate-y-[-10px]">
+      <div
+        className="container relative group text-slate-100 p-6 border rounded-md border-slate-500 shadow-lg 
+      flex flex-col items-center justify-around gap-4 bg-black/60 transform hover:translate-y-[-10px]"
+      >
         <div>
           <Image
             className="object-cover"
@@ -57,10 +59,12 @@ const ClientMenu = (menuList) => {
         <p className="text-sm font-sans text-center text-gray-300">
           {menuList.description}
         </p>
-        
+
         {/* below is for hover transition/animation/opacity */}
-        <div className="absolute inset-0 flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity
-        rounded-md bg-black bg-opacity-50">
+        <div
+          className="absolute inset-0 flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity
+        rounded-md bg-black bg-opacity-50"
+        >
           <button
             className="bg-yellow-400 text-black px-6 py-2 rounded button-animation"
             onClick={handleAddToCartClick}
