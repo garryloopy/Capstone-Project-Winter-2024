770accb98ca6f06edc1a448f848aa41d786a357e
Rimon83 <111208690+Rimon83@users.noreply.github.com>
2024-04-01 15:59:33 -0600

    update subheading component by adding spoon icon and style the icon

--- components/SubHeader.js
+++ components/SubHeader.js
@@ -1,17 +1,30 @@
 import React from "react";
+import { FaSpoon } from "react-icons/fa6";
+
 
 const SubHeader = ({ header2 }) => {
   return (
-    <div className="my-[2rem] flex flex-row items-center gap-3">
-      <img
+    <div className="my-[2rem] flex flex-row items-center gap-6">
+      {/* <img
         src="/images/spoon.png"
         alt="spoon"
-        className="w-[45px] rotate-180"
+        className="w-[60px] rotate-180"
+      /> */}
+
+      {/* use spoon react icon, we can change the color or size of the icon */}
+      <FaSpoon
+        size={45}
+        style={{ transform: "rotate(225deg)", color: "#58A399" }}
       />
       <p className="md:text-4xl text-2xl font-extrabold text-violet-500/95 tracking-wider	mb-[.5rem]">
         {header2}
       </p>
-      <img src="/images/spoon.png" alt="spoon" className="w-[45px]" />
+      {/* <img src="/images/spoon.png" alt="spoon" className="w-[60px]" /> */}
+
+      <FaSpoon
+        size={45}
+        style={{ transform: "rotate(45deg)", color: "#58A399" }}
+      />
     </div>
   );
 };
