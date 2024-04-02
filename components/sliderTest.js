08429fcf737d5fb51de18940e43d87f8383966bd
Rimon83 <111208690+Rimon83@users.noreply.github.com>
2024-04-01 19:26:31 -0600

    update sliderTester and slide.css

--- components/sliderTest.js
+++ components/sliderTest.js
@@ -2,60 +2,79 @@ import React, { useEffect, useRef, useState } from "react";
 
 // use whatever images you want but include the path
 const imagePaths = [
-  "images/gallery01.png", 
-  "images/gallery02.png", 
+  "images/gallery01.png",
+  "images/gallery02.png",
   "images/gallery03.png",
   "images/background.jpg",
-  "images/Lord_farquaad.jpg"
+  "images/Lord_farquaad.jpg",
 ];
 
 function Slideshow() {
-  const slideshowRef = useRef(null);
-  const [activeIndex, setActiveIndex] = useState(0);
+  const [slider, setSlider] = useState(0);
 
-  useEffect(() => {
-    const slideshowInterval = setInterval(() => {
-      const currentScrollLeft = slideshowRef.current.scrollLeft;
-      const slideWidth = slideshowRef.current.offsetWidth;
-      const maxScrollLeft = slideshowRef.current.scrollWidth - slideWidth;
+  // const slideshowRef = useRef(null);
+  // const [activeIndex, setActiveIndex] = useState(0);
 
-      if (currentScrollLeft === maxScrollLeft) {
-        slideshowRef.current.scrollTo({ left: 0, behavior: "smooth" });
-      } else {
-        slideshowRef.current.scrollBy({ left: slideWidth, behavior: "smooth" });
-      }
-    }, 3000); // --- this is to set the timer (in milliseconds) ---
+  // useEffect(() => {
+  //   const slideshowInterval = setInterval(() => {
+  //     const currentScrollLeft = slideshowRef.current.scrollLeft;
+  //     const slideWidth = slideshowRef.current.offsetWidth;
+  //     const maxScrollLeft = slideshowRef.current.scrollWidth - slideWidth;
 
-    return () => clearInterval(slideshowInterval);
-  }, []);
-  
-  // to make the dots clickable
-  const handleOnClick = (index) => {
-    setActiveIndex(index);
-    slideshowRef.current.scrollTo({
-      left: index * slideshowRef.current.offsetWidth,
-      behavior: "smooth",
-    });
+  //     if (currentScrollLeft === maxScrollLeft) {
+  //       slideshowRef.current.scrollTo({ left: 0, behavior: "smooth" });
+  //     } else {
+  //       slideshowRef.current.scrollBy({ left: slideWidth, behavior: "smooth" });
+  //     }
+  //   }, 3000); // --- this is to set the timer (in milliseconds) ---
+
+  //   return () => clearInterval(slideshowInterval);
+  // }, []);
+
+  // other way: use setTimeout
+  const nextSlide = () => {
+    setSlider(slider === imagePaths.length - 1 ? 0 : slider + 1);
   };
+  useEffect(() => {
+      setTimeout(() => {
+        nextSlide();
+      }, 3000);
+  }, [nextSlide]);
+
+ 
+
+  // to make the dots clickable
+  // const handleOnClick = (index) => {
+  //   setActiveIndex(index);
+  //   slideshowRef.current.scrollTo({
+  //     left: index * slideshowRef.current.offsetWidth,
+  //     behavior: "smooth",
+  //   });
+  // };
 
   return (
     <div className="slideshow">
-      <div className="slider" ref={slideshowRef}>
+      <div className="slider">
         {imagePaths.map((imagePath, index) => (
-          <div className="slide" key={index}>
+          <div
+            className={slider === index ? "slide" : "slide slide-hidden"}
+            key={index}
+          >
             <img src={imagePath} alt={`Slide ${index + 1}`} />
           </div>
         ))}
       </div>
-      {/* <div className="slideshowDots">
+      <div className="slideshowDots">
         {imagePaths.map((_, index) => (
           <div
             key={index}
-            className={`slideshowDot ${index === activeIndex ? "active" : ""}`}
-            onClick={() => handleOnClick(index)}
+            className={
+              slider === index ? "slideshowDot" : "slideshowDot activeDot"
+            }
+            onClick={() => setSlider(index)}
           />
         ))}
-      </div> */}
+      </div>
     </div>
   );
 }
