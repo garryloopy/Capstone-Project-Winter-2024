e4ca8219c056225a5b6ef00e6906a247f2c4d44d
Garry Jr Dayag <garrydayagjr@gmail.com>
2024-04-04 15:29:38 -0600

    Fix bug with header and navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -6,40 +6,41 @@ import { useScrollTop } from "@/app/hooks/use-scroll-top";
 
 const Navbar = () => {
   const path = usePathname();
-  const scrolled = useScrollTop()
+  const scrolled = useScrollTop();
 
   return (
     <nav
-      className={`lg:flex hidden justify-center items-center font-semibold text-md ${
-        scrolled ? "text-white" : "text-slate-800 "
-      }`}
+      className={`lg:flex hidden justify-center items-center font-semibold text-md `}
     >
       <Link
         href="/"
+        data-scrolled={scrolled}
         className={`hover:opacity-100 transition-opacity duration-500 min-w-40 py-2 grid place-items-center ${
           path === "/"
             ? "opacity-100 bg-yellow-400 rounded-3xl shadow-md"
-            : "opacity-50"
+            : "opacity-70 data-[scrolled=true]:text-white"
         }`}
       >
         Home
       </Link>
       <Link
         href="/menu"
+        data-scrolled={scrolled}
         className={`hover:opacity-100 transition-opacity duration-500 min-w-40 py-2 grid place-items-center ${
           path === "/menu"
             ? "opacity-100 bg-yellow-400 rounded-3xl shadow-md"
-            : "opacity-50"
+            : "opacity-70 data-[scrolled=true]:text-white"
         }`}
       >
         Menu
       </Link>
       <Link
         href="/contact"
+        data-scrolled={scrolled}
         className={`hover:opacity-100 transition-opacity duration-500 min-w-40 py-2 grid place-items-center ${
           path === "/contact"
             ? "opacity-100 bg-yellow-400 rounded-3xl shadow-md"
-            : "opacity-50"
+            : "opacity-70 data-[scrolled=true]:text-white"
         }`}
       >
         Contact Us
