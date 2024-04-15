b1ea2d4317bde252e809313a15c52acc858d46df
Kevin <homingkevin.wong@edu.sait.ca>
2024-04-14 21:33:11 -0600

    Remove unnecessary imports

--- app/(admin)/statistic/page.js
+++ app/(admin)/statistic/page.js
@@ -1,24 +1,14 @@
 "use client";
 import SubHeader from "@/components/SubHeader";
-import React, { useEffect, useState, Component } from "react";
+import React, { useState } from "react";
 import AdminNavbar from "../components/AdminNavbar";
-import { useSession } from "next-auth/react";
-import { redirect, usePathname } from "next/navigation";
-import Loading from "@/components/Loading";
-import MenuForm from "../components/MenuForm";
-import toast, { Toaster } from "react-hot-toast";
-import SingleMenu from "../components/SingleMenu";
-import Link from "next/link";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPenToSquare, faTrash } from "@fortawesome/free-solid-svg-icons";
-import { withSwal } from "react-sweetalert2";
-import { deletePhoto } from "@/actions/uploadImage";
+import { usePathname } from "next/navigation";
 import MenuScroll from "@/components/MenuScroll";
 
 import OrderChart from "./OrderChart";
 import ProfitChart from "./ProfitChart";
 
-export default function statistic() {
+const statistic = () => {
   const path = usePathname();
 
   const [selectedChart, setSelectedChart] = useState("OrderChart");
@@ -56,4 +46,6 @@ export default function statistic() {
       </section>
     </>
   );
-}
+};
+
+export default statistic;
