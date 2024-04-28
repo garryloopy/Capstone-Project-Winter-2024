00e027799975b4c7be7aa37202237dc00fc4bbda
Kevin <homingkevin.wong@edu.sait.ca>
2024-04-28 11:32:27 -0600

    .

--- app/(admin)/statistic/OrderChart.js
+++ app/(admin)/statistic/OrderChart.js
@@ -1,120 +1,109 @@
 import React, { useEffect, useState } from "react";
 import { Bar } from "react-chartjs-2";
-import {
-  Chart,
-  CategoryScale,
-  LinearScale,
-  BarElement,
-  Title,
-  Tooltip,
-} from "chart.js";
+import { Chart, CategoryScale, LinearScale, BarElement, Title, Tooltip } from "chart.js";
 
 Chart.register(CategoryScale, LinearScale, BarElement, Title, Tooltip);
 
 export default function OrderChart() {
-  const options = {
-    scales: {
-      yAxes: [{ ticks: { beginAtZero: true, stepSize: 1 } }],
-    },
-    plugins: {
-      title: {
-        display: true,
-        text: "Monthly Order Chart",
-        font: {
-          size: 20,
-        },
-      },
-    },
-    indexAxis: "x",
-    barThickness: 50,
-  };
+	const options = {
+		scales: {
+			yAxes: [{ ticks: { beginAtZero: true, stepSize: 1 } }],
+		},
+		plugins: {
+			title: {
+				display: true,
+				text: "Monthly Order Chart",
+				font: {
+					size: 20,
+				},
+			},
+		},
+		indexAxis: "x",
+		barThickness: 50,
+	};
 
-  const [graphData, setGraphData] = useState({
-    labels: [],
-    datasets: [
-      {
-        label: null,
-        data: [],
-        backgroundColor: "Orange",
-        borderWidth: 1,
-      },
-    ],
-  });
+	const [graphData, setGraphData] = useState({
+		labels: [],
+		datasets: [
+			{
+				label: null,
+				data: [],
+				backgroundColor: "Orange",
+				borderWidth: 1,
+			},
+		],
+	});
 
-  useEffect(() => {
-    fetchOrderData();
-  }, []);
+	useEffect(() => {
+		fetchOrderData();
+	}, []);
 
-  const fetchOrderData = async () => {
-    // Get 30 days of data
-    const currentDate = new Date();
-    const previousMonth = new Date(currentDate);
-    previousMonth.setDate(previousMonth.getDate() - 1);
+	const fetchOrderData = async () => {
+		// Get 30 days of data
+		const currentDate = new Date();
+		const previousMonth = new Date(currentDate);
+		previousMonth.setDate(previousMonth.getDate() - 1);
 
-    const daysInPreviousMonth = new Date(
-      previousMonth.getFullYear(),
-      previousMonth.getMonth() + 1,
-      0
-    ).getDate();
+		const daysInPreviousMonth = new Date(
+			previousMonth.getFullYear(),
+			previousMonth.getMonth() + 1,
+			0
+		).getDate();
 
-    const startDate = new Date(
-      previousMonth.getFullYear(),
-      previousMonth.getMonth(),
-      1
-    );
+		const startDate = new Date(previousMonth.getFullYear(), previousMonth.getMonth(), 1);
 
-    const endDate = new Date(
-      previousMonth.getFullYear(),
-      previousMonth.getMonth(),
-      daysInPreviousMonth
-    );
+		const endDate = new Date(
+			previousMonth.getFullYear(),
+			previousMonth.getMonth(),
+			daysInPreviousMonth
+		);
 
-    try {
-      const res = await fetch("/api/getOrderList");
-      if (res.ok) {
-        const orders = await res.json();
-        const orderCounts = {};
+		try {
+			const res = await fetch("/api/getOrderList");
+			if (res.ok) {
+				const orders = await res.json();
+				const orderCounts = {};
 
-        orders.forEach((order) => {
-          const orderDate = new Date(order.createdAt);
-          if (orderDate >= startDate && orderDate <= endDate) {
-            const formattedDate = new Intl.DateTimeFormat("en-US", {
-              month: "short",
-              day: "2-digit",
-            }).format(new Date(order.createdAt));
+				orders.forEach((order) => {
+					const orderDate = new Date(order.createdAt);
+					if (orderDate >= startDate && orderDate <= endDate) {
+						const formattedDate = new Intl.DateTimeFormat("en-US", {
+							month: "short",
+							day: "2-digit",
+						}).format(new Date(order.createdAt));
 
-            orderCounts[formattedDate] = (orderCounts[formattedDate] || 0) + 1;
-          }
-        });
+						orderCounts[formattedDate] = (orderCounts[formattedDate] || 0) + 1;
+					}
+				});
 
-        const labels = Object.keys(orderCounts);
-        const data = labels.map((label) => orderCounts[label]);
+				const labels = Object.keys(orderCounts);
+				const data = labels.map((label) => orderCounts[label]);
 
-        const graphData = {
-          labels: labels,
-          datasets: [
-            {
-              label: "Order(s)",
-              data: data,
-              backgroundColor: "Orange",
-              borderWidth: 1,
-            },
-          ],
-        };
-        setGraphData(graphData);
-      } else {
-        console.log("Error fetching order data");
-      }
-    } catch (error) {
-      console.error("An error occurred while fetching order data:", error);
-    }
-  };
+				const graphData = {
+					labels: labels,
+					datasets: [
+						{
+							label: "Order(s)",
+							data: data,
+							backgroundColor: "Orange",
+							borderWidth: 1,
+						},
+					],
+				};
+				setGraphData(graphData);
+			} else {
+				console.log("Error fetching order data");
+			}
+		} catch (error) {
+			console.error("An error occurred while fetching order data:", error);
+		}
+	};
 
-  return (
-    <section>
-      <div id="order-chart">
-        <Bar data={graphData} options={options} />
-      </div>
-    </section>
-  );
+	return (
+		<section>
+			<div id="order-chart">
+				<Bar data={graphData} options={options} />
+			</div>
+		</section>
+	);
 }
--- app/(admin)/statistic/ProfitChart.js
+++ app/(admin)/statistic/ProfitChart.js
@@ -1,137 +1,125 @@
 import React, { useEffect, useState } from "react";
 import { Line } from "react-chartjs-2";
 import {
-  Chart,
-  CategoryScale,
-  LinearScale,
-  LineElement,
-  PointElement,
-  Title,
-  Tooltip,
+	Chart,
+	CategoryScale,
+	LinearScale,
+	LineElement,
+	PointElement,
+	Title,
+	Tooltip,
 } from "chart.js";
 
-Chart.register(
-  CategoryScale,
-  LinearScale,
-  LineElement,
-  PointElement,
-  Title,
-  Tooltip
-);
+Chart.register(CategoryScale, LinearScale, LineElement, PointElement, Title, Tooltip);
 
 export default function ProfitChart() {
-  const options = {
-    scales: {
-      yAxes: [{ ticks: { beginAtZero: true } }],
-    },
-    plugins: {
-      title: {
-        display: true,
-        text: "Monthly Profit Chart",
-        font: {
-          size: 20,
-        },
-      },
-    },
-  };
+	const options = {
+		scales: {
+			yAxes: [{ ticks: { beginAtZero: true } }],
+		},
+		plugins: {
+			title: {
+				display: true,
+				text: "Monthly Profit Chart",
+				font: {
+					size: 20,
+				},
+			},
+		},
+	};
 
-  const [graphData, setGraphData] = useState({
-    labels: [],
-    datasets: [
-      {
-        label: "Profit",
-        data: [],
-        borderColor: "Orange",
-        borderWidth: 2,
-        fill: true,
-      },
-    ],
-  });
+	const [graphData, setGraphData] = useState({
+		labels: [],
+		datasets: [
+			{
+				label: "Profit",
+				data: [],
+				borderColor: "Orange",
+				borderWidth: 2,
+				fill: true,
+			},
+		],
+	});
 
-  useEffect(() => {
-    fetchProfitData();
-  }, []);
+	useEffect(() => {
+		fetchProfitData();
+	}, []);
 
-  const fetchProfitData = async () => {
-    try {
-      // Get 30 days of data
-      const currentDate = new Date();
-      const previousMonth = new Date(currentDate);
-      previousMonth.setDate(previousMonth.getMonth() - 1);
+	const fetchProfitData = async () => {
+		// Get 30 days of data
+		const currentDate = new Date();
+		const previousMonth = new Date(currentDate);
+		previousMonth.setDate(previousMonth.getMonth() - 1);
 
-      const daysInPreviousMonth = new Date(
-        previousMonth.getFullYear(),
-        previousMonth.getMonth() + 1,
-        0
-      ).getDate();
+		const daysInPreviousMonth = new Date(
+			previousMonth.getFullYear(),
+			previousMonth.getMonth() + 1,
+			0
+		).getDate();
 
-      const startDate = new Date(
-        previousMonth.getFullYear(),
-        previousMonth.getMonth(),
-        1
-      );
+		const startDate = new Date(previousMonth.getFullYear(), previousMonth.getMonth(), 1);
 
-      const endDate = new Date(
-        previousMonth.getFullYear(),
-        previousMonth.getMonth(),
-        daysInPreviousMonth
-      );
+		const endDate = new Date(
+			previousMonth.getFullYear(),
+			previousMonth.getMonth(),
+			daysInPreviousMonth
+		);
 
-      const res = await fetch("/api/getOrderList");
-      if (res.ok) {
-        const orders = await res.json();
-        const orderAmount = {};
+		try {
+			const res = await fetch("/api/getOrderList");
+			if (res.ok) {
+				const orders = await res.json();
+				const orderAmount = {};
 
-        orders.forEach((order) => {
-          const orderDate = new Date(order.createdAt);
-          if (orderDate >= startDate && orderDate <= endDate) {
-            const formattedDate = new Intl.DateTimeFormat("en-US", {
-              month: "short",
-              day: "2-digit",
-            }).format(orderDate);
+				orders.forEach((order) => {
+					const orderDate = new Date(order.createdAt);
+					if (orderDate >= startDate && orderDate <= endDate) {
+						const formattedDate = new Intl.DateTimeFormat("en-US", {
+							month: "short",
+							day: "2-digit",
+						}).format(orderDate);
 
-            let totalAmount = 0;
+						let totalAmount = 0;
 
-            if (order.cartProducts && Array.isArray(order.cartProducts)) {
-              order.cartProducts.forEach((product) => {
-                const price = parseFloat(product.price.replace("$", ""));
-                if (!isNaN(price)) {
-                  totalAmount += price;
-                }
-              });
-            }
+						if (order.cartProducts && Array.isArray(order.cartProducts)) {
+							order.cartProducts.forEach((product) => {
+								const price = parseFloat(product.price.replace("$", ""));
+								if (!isNaN(price)) {
+									totalAmount += price;
+								}
+							});
+						}
 
-            orderAmount[formattedDate] =
-              (orderAmount[formattedDate] || 0) + totalAmount;
-          }
-        });
+						orderAmount[formattedDate] = (orderAmount[formattedDate] || 0) + totalAmount;
+					}
+				});
 
-        const labels = Object.keys(orderAmount);
-        const data = labels.map((label) => orderAmount[label]);
+				const labels = Object.keys(orderAmount);
+				const data = labels.map((label) => orderAmount[label]);
 
-        const newGraphData = {
-          labels: labels,
-          datasets: [
-            {
-              ...graphData.datasets[0],
-              data: data,
-            },
-          ],
-        };
-        setGraphData(newGraphData);
-      } else {
-        console.log("Error fetching order data");
-      }
-    } catch (error) {
-      console.error("An error occurred while fetching order data:", error);
-    }
-  };
+				const newGraphData = {
+					labels: labels,
+					datasets: [
+						{
+							...graphData.datasets[0],
+							data: data,
+						},
+					],
+				};
+				setGraphData(newGraphData);
+			} else {
+				console.log("Error fetching order data");
+			}
+		} catch (error) {
+			console.error("An error occurred while fetching order data:", error);
+		}
+	};
 
-  return (
-    <section>
-      <div id="profit-chart">
-        <Line data={graphData} options={options} />
-      </div>
-    </section>
-  );
+	return (
+		<section>
+			<div id="profit-chart">
+				<Line data={graphData} options={options} />
+			</div>
+		</section>
+	);
 }
