23ab031b8e0e667c153aa2b9a63290444568de67
Edward Montilla <edward.montilla@edu.sait.ca>
2024-05-15 18:26:53 -0600

    added backdrop-blur in sign-in box

--- app/(admin)/components/AdminModal.js
+++ app/(admin)/components/AdminModal.js
@@ -13,7 +13,7 @@ const AdminModal = () => {
   return (
     <>
       {showModal && (
-        <div className="fixed bottom-0 right-[.1rem] w-[30rem] h-48 bg-gray-800/95 lg:mr-4 lg:mb-4 text-gray-50 border border-gray-600 rounded-md">
+        <div className="fixed bottom-0 right-[.1rem] w-[30rem] h-48 bg-gray-800/50 backdrop-blur-sm lg:mr-4 lg:mb-4 text-gray-50 border border-gray-600 rounded-md">
           {/* Close button for top right  */}
           <button
             className="absolute grid rounded-md top-6 right-6 size-7 place-content-center hover:bg-slate-100 text-slate-50 hover:text-slate-800"
